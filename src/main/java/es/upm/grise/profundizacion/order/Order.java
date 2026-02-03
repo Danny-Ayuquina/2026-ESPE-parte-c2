8fea227a9210cfb8c5e52ce283ec8dd6b8298549
Danny-Ayuquina <dmayuquina@espe.edu.ec>
2026-02-03 09:35:31 -0500

    Implementacion de métodos Order

--- src/main/java/es/upm/grise/profundizacion/order/Order.java
+++ src/main/java/es/upm/grise/profundizacion/order/Order.java
@@ -1,33 +1,46 @@
 package es.upm.grise.profundizacion.order;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
 public class Order {
 
     private Collection<Item> items;
 
-	/*
-	 * Constructor
-	 */
+    /*
+     * Constructor
+     */
     public Order() {
-        
+        this.items = new ArrayList<>();
     }
 
-	/*
-	 * Method to code / test
-	 */
+    /*
+     * Method to code / test
+     */
     public void addItem(Item item) {
 
+        for (Item existingItem : items) {
+
+            long existingProductId = existingItem.getProduct().getId();
+            long newProductId = item.getProduct().getId();
+
+            if (existingProductId == newProductId) {
+                // If product already exists, increase quantity
+                existingItem.setQuantity(
+                    existingItem.getQuantity() + item.getQuantity()
+                );
+                return;
+            }
+        }
+
+        // If product does not exist, add new item
+        items.add(item);
     }
-    
-	/*
-	 * Setters/getters
-	 */
+
+    /*
+     * Setters/getters
+     */
     public Collection<Item> getItems() {
-    	
-    	return this.items;
-    	
+        return this.items;
     }
-
 }
-
